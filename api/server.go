439fdb262991d0e61f03e5bb3ac0f7dae2c3e107
arodrigow <rodrigow_@hotmail.com.br>
2023-10-02 12:57:57 -0300

    Added [Account] Create, Get by ID add Get all routes

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.comarodrigowsimple_bank/db/sqlc"
+)
+
+// Server serves HTTP requests for the banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccountById)
+	router.GET("/accounts", server.getAllAccounts)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
